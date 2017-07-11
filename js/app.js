f2b44969701f3c67c815f3103100ecb9a842ea2b
一像素 <mobileyou@qq.com>
2017-07-11 16:25:09 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -130,6 +130,45 @@
                     }.bind(this),100);
                 }
            }.bind(this),false);
+            
+           /*当touch事件被取消时*/
+           document.addEventListener("touchcancel",function(e){
+               e.preventDefault();
+               var translate = 0;
+               //计算手指在屏幕上停留的时间
+               var deltaT = new Date().getTime() - startT;
+               if (isMove){ //发生了左右滑动
+                    //使用动画过渡让页面滑动到最终的位置
+                    viewport.style.webkitTransition = "0.3s ease -webkit-transform";
+                    if(deltaT < 300){ //如果停留时间小于300ms,则认为是快速滑动，无论滑动距离是多少，都停留到下一页
+                        translate = direction == 'left'?
+                        currentPosition-(pageWidth+moveLength):currentPosition+pageWidth-moveLength;
+                        //如果最终位置超过边界位置，则停留在边界位置
+                        translate = translate > 0 ? 0 : translate; //左边界
+                        translate = translate < maxWidth ? maxWidth : translate; //右边界
+                    }else {
+                        //如果滑动距离小于屏幕的50%，则退回到上一页
+                        if (Math.abs(moveLength)/pageWidth < 0.5){
+                            translate = currentPosition-moveLength;
+                        }else{
+                            //如果滑动距离大于屏幕的50%，则滑动到下一页
+                            translate = direction == 'left'?
+                            currentPosition-(pageWidth+moveLength):currentPosition+pageWidth-moveLength;
+                            translate = translate > 0 ? 0 : translate;
+                            translate = translate < maxWidth ? maxWidth : translate;
+                        }
+                    }
+                    //执行滑动，让页面完整的显示到屏幕上
+                    this.transform.call(viewport,translate);
+                    //计算当前的页码
+                    pageNow = Math.round(Math.abs(translate) / pageWidth) + 1;
+
+                    setTimeout(function(){
+                        //设置页码，DOM操作需要放到子线程中，否则会出现卡顿
+                        this.setPageNow();
+                    }.bind(this),100);
+                }
+           }.bind(this),false); 
        }
     }
 })(window,document);
