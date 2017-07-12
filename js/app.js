1802ef56c1e0cb4640e566057e4b7108d9536683
一像素 <mobileyou@qq.com>
2017-07-12 12:52:37 +0800

    修复：多手指操作触发手势事件而引起的BUG

--- js/app.js
+++ js/app.js
@@ -23,9 +23,7 @@
             button.addEventListener('touchstart',function(){
                 console.log('touch')
             })
-            //button.addEventListener('click',function(){
-            //    console.log('click')
-            //})
+             
         },
 
 
@@ -74,13 +72,14 @@
                    viewport.style.webkitTransition = ""; //取消动画效果
                    startT = new Date().getTime(); //记录手指按下的开始时间
                    isMove = false; //是否产生滑动
-                   isTouchEnd = false; //滑动开始
+                   isTouchEnd = false; //当前滑动开始
                }
            }.bind(this),false);
 
            /*手指在屏幕上滑动，页面跟随手指移动*/
            document.addEventListener("touchmove",function(e){
                e.preventDefault();
+               
                //如果当前滑动已结束，不管其他手指是否在屏幕上都禁止该事件
                if(isTouchEnd) return ;
                
@@ -131,6 +130,7 @@
                             translate = translate < maxWidth ? maxWidth : translate;
                         }
                     }
+                   
                     //执行滑动，让页面完整的显示到屏幕上
                     this.transform.call(viewport,translate);
                     //计算当前的页码
@@ -142,45 +142,6 @@
                     }.bind(this),100);
                 }
            }.bind(this),false);
-            
-           /*当touch事件被取消时*/
-           document.addEventListener("touchcancel",function(e){
-               e.preventDefault();
-               var translate = 0;
-               //计算手指在屏幕上停留的时间
-               var deltaT = new Date().getTime() - startT;
-               if (isMove){ //发生了左右滑动
-                    //使用动画过渡让页面滑动到最终的位置
-                    viewport.style.webkitTransition = "0.3s ease -webkit-transform";
-                    if(deltaT < 300){ //如果停留时间小于300ms,则认为是快速滑动，无论滑动距离是多少，都停留到下一页
-                        translate = direction == 'left'?
-                        currentPosition-(pageWidth+moveLength):currentPosition+pageWidth-moveLength;
-                        //如果最终位置超过边界位置，则停留在边界位置
-                        translate = translate > 0 ? 0 : translate; //左边界
-                        translate = translate < maxWidth ? maxWidth : translate; //右边界
-                    }else {
-                        //如果滑动距离小于屏幕的50%，则退回到上一页
-                        if (Math.abs(moveLength)/pageWidth < 0.5){
-                            translate = currentPosition-moveLength;
-                        }else{
-                            //如果滑动距离大于屏幕的50%，则滑动到下一页
-                            translate = direction == 'left'?
-                            currentPosition-(pageWidth+moveLength):currentPosition+pageWidth-moveLength;
-                            translate = translate > 0 ? 0 : translate;
-                            translate = translate < maxWidth ? maxWidth : translate;
-                        }
-                    }
-                    //执行滑动，让页面完整的显示到屏幕上
-                    this.transform.call(viewport,translate);
-                    //计算当前的页码
-                    pageNow = Math.round(Math.abs(translate) / pageWidth) + 1;
-
-                    setTimeout(function(){
-                        //设置页码，DOM操作需要放到子线程中，否则会出现卡顿
-                        this.setPageNow();
-                    }.bind(this),100);
-                }
-           }.bind(this),false); 
        }
     }
 })(window,document);
